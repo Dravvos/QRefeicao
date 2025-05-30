8a3a5ea49b1fc89671f0682ec8dde9a876a1de25
Dravvos <daniddias53@gmail.com>
2025-05-30 17:27:00 -0300

    Criação das controllers

---
+++ QRefeicao.API/Controllers/CardapioController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QRefeicao.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardapioController : ControllerBase
+    {
+    }
+}
