3943d6f830da1f85060d8bb65b17f1a332d75f6d
Dravvos <daniddias53@gmail.com>
2025-09-16 22:58:04 -0300

    Ajustes

--- QRefeicao.Data.NoSQL/MongoDbContext.cs
+++ QRefeicao.Data.NoSQL/MongoDbContext.cs
@@ -16,44 +16,39 @@ namespace QRefeicao.Data.NoSQL
         }
         public MongoDbContext()
         {
-            try
+            var mongoConnectionString = Environment.GetEnvironmentVariable("MongoDBConnection");
+            var settings = MongoClientSettings.FromUrl(new MongoUrl(mongoConnectionString));
+
+            string crtPath = @"C:\Users\supero\mongodb-ca.crt";
+            string pfxPath = @"C:\Users\supero\mongodb-client.pfx";
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
             {
-                var mongoConnectionString = Environment.GetEnvironmentVariable("MongoDBConnection");
-                var settings = MongoClientSettings.FromUrl(new MongoUrl(mongoConnectionString));
+                crtPath = "/etc/mongodb/certs/mongodb-ca.crt";
+                pfxPath = "/etc/mongodb/certs/mongodb-client.pfx";
 
-                string crtPath = @"C:\Users\supero\mongodb-ca.crt";
-                string pfxPath = @"C:\Users\supero\mongodb-client.pfx";
-                if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
-                {
-                    crtPath = "/etc/mongodb/certs/mongodb-ca.crt";
-                    pfxPath = "/etc/mongodb/certs/mongodb-client.pfx";
-
-                    _logger.LogDebug("Ambiente de produção detectado");
-                }
-                else
-                {
-                    _logger.LogDebug("Ambiente de desenvolvimento detectado, utilizando certificados locais.");
-                }
-                    var caCert = new X509Certificate2(crtPath);
-                var clientCert = new X509Certificate2(pfxPath, Environment.GetEnvironmentVariable("CertificatePassword"),
-                     X509KeyStorageFlags.MachineKeySet |
-                X509KeyStorageFlags.PersistKeySet |
-                X509KeyStorageFlags.Exportable);
-
-                settings.SslSettings = new SslSettings
-                {
-                    ClientCertificates = new List<X509Certificate> { clientCert },
-                    ServerCertificateValidationCallback = (sender, cert, chain, errors) => true
-                };
-
-                var client = new MongoClient(settings);
-
-                _database = client.GetDatabase("QRefeicao");
+                _logger.LogInformation("Ambiente de produção detectado");
             }
-            catch (Exception ex)
+            else
             {
-                _logger.LogError("Erro ao conectar no mongo: " + ex.Message);
-            }            
+                _logger.LogInformation("Ambiente de desenvolvimento detectado, utilizando certificados locais.");
+            }
+            var caCert = new X509Certificate2(crtPath);
+            var clientCert = new X509Certificate2(pfxPath, Environment.GetEnvironmentVariable("CertificatePassword"),
+                 X509KeyStorageFlags.MachineKeySet |
+            X509KeyStorageFlags.PersistKeySet |
+            X509KeyStorageFlags.Exportable);
+
+            settings.SslSettings = new SslSettings
+            {
+                ClientCertificates = new List<X509Certificate> { clientCert },
+                ServerCertificateValidationCallback = (sender, cert, chain, errors) => true
+            };
+
+            var client = new MongoClient(settings);
+
+            _database = client.GetDatabase("QRefeicao");
+
+
         }
 
         public IMongoCollection<TRADUCAODTC_NOSQL> Traducao => _database.GetCollection<TRADUCAODTC_NOSQL>("Traducao");
