75de9dfb303a05e0108b9144fff6b74e6d12b5df
Dravvos <daniddias53@gmail.com>
2025-09-16 21:21:17 -0300

    Ajuste conexão mongo

--- QRefeicao.Data.NoSQL/MongoDbContext.cs
+++ QRefeicao.Data.NoSQL/MongoDbContext.cs
@@ -10,31 +10,38 @@ namespace QRefeicao.Data.NoSQL
 
         public MongoDbContext()
         {
-            var mongoConnectionString = Environment.GetEnvironmentVariable("MongoDBConnection");
-            var settings = MongoClientSettings.FromUrl(new MongoUrl(mongoConnectionString));
-            
-            string crtPath = @"C:\Users\supero\mongodb-ca.crt";
-            string pfxPath = @"C:\Users\supero\mongodb-client.pfx";
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            try
             {
-                crtPath = "/etc/mongodb/certs/mongodb-ca.crt";
-                pfxPath = "/etc/mongodb/certs/mongodb-client.pfx";
+                var mongoConnectionString = Environment.GetEnvironmentVariable("MongoDBConnection");
+                var settings = MongoClientSettings.FromUrl(new MongoUrl(mongoConnectionString));
+
+                string crtPath = @"C:\Users\supero\mongodb-ca.crt";
+                string pfxPath = @"C:\Users\supero\mongodb-client.pfx";
+                if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+                {
+                    crtPath = "/etc/mongodb/certs/mongodb-ca.crt";
+                    pfxPath = "/etc/mongodb/certs/mongodb-client.pfx";
+                }
+                var caCert = new X509Certificate2(crtPath);
+                var clientCert = new X509Certificate2(pfxPath, Environment.GetEnvironmentVariable("CertificatePassword"),
+                     X509KeyStorageFlags.MachineKeySet |
+                X509KeyStorageFlags.PersistKeySet |
+                X509KeyStorageFlags.Exportable);
+
+                settings.SslSettings = new SslSettings
+                {
+                    ClientCertificates = new List<X509Certificate> { clientCert },
+                    ServerCertificateValidationCallback = (sender, cert, chain, errors) => true
+                };
+
+                var client = new MongoClient(settings);
+
+                _database = client.GetDatabase("QRefeicao");
             }
-            var caCert = new X509Certificate2(crtPath);
-            var clientCert = new X509Certificate2(pfxPath, Environment.GetEnvironmentVariable("CertificatePassword"),
-                 X509KeyStorageFlags.MachineKeySet |
-            X509KeyStorageFlags.PersistKeySet |
-            X509KeyStorageFlags.Exportable);
-
-            settings.SslSettings = new SslSettings
+            catch (Exception ex)
             {
-                ClientCertificates = new List<X509Certificate> { clientCert },
-                ServerCertificateValidationCallback = (sender, cert, chain, errors) => true
-            };
-
-            var client = new MongoClient(settings);
-
-            _database = client.GetDatabase("QRefeicao");
+                Console.WriteLine("Erro ao conectar no mongo: " + ex.Message);
+            }            
         }
 
         public IMongoCollection<TRADUCAODTC_NOSQL> Traducao => _database.GetCollection<TRADUCAODTC_NOSQL>("Traducao");
